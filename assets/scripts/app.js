4986818f1c757f9efe9983caaeee5c32f3eacc80
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-10-02 20:28:15 +0200

    assignment #1

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,13 @@
-alert('I am here')
+const defaltResult = 0;
+let currentResult = defaltResult;
+let userInput = 10;
+
+currentResult = userInput + 18;
+currentResult = currentResult - 10;
+currentResult = currentResult * 2;
+currentResult = currentResult / 8;
+
+alert(currentResult);
+alert(userInput)
+
+
