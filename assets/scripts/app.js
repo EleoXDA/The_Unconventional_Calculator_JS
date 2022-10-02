96c0b1f3434763f395d0c0ba4b2b117dc1ab1a0f
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-10-02 20:34:31 +0200

    Escape const error

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,11 +1,11 @@
 const defaltResult = 0;
 let currentResult = defaltResult;
-let userInput = 10;
+let userInputValue = 10;
 
-currentResult = userInput + 18;
+currentResult = userInputValue + 18;
 currentResult = currentResult - 10;
 currentResult = currentResult * 2;
 currentResult = currentResult / 8;
 
 alert(currentResult);
-alert(userInput)
+alert(userInputValue)
