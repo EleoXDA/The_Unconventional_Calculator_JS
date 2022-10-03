0c892069c0c7b010692461dd48f8178a2b2e7dee
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-10-03 22:57:05 +0200

    ADD subtraction function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -13,5 +13,13 @@ function addition() {
   outputResult(currentResult, calcDescription);
 }
 
+function subtraction() {
+  const enteredNumber = getUserNumberInput();
+  const calcDescription = `${currentResult} - ${enteredNumber}`; // it implicitly converts the value to string 
+  // we also need to run it before the calculation so that we can get the previous currentResult
+  currentResult = currentResult - enteredNumber; // parseFloat() could also be used but input cannot be a number with decimal points
+  outputResult(currentResult, calcDescription);
+}
+
 addBtn.addEventListener("click", addition);
 
