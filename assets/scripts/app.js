061c5ecfee68793a68ea4c8f9dda9b45ccc36ab2
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-10-03 22:55:09 +0200

    to avoid code repetition #2

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,12 @@
 const defaltResult = 0;
 let currentResult = defaltResult;
 
+function getUserNumberInput() {
+  return parseInt(userInput.value);
+}
+
 function addition() {
-  const enteredNumber = parseInt(userInput.value);
+  const enteredNumber = getUserNumberInput();
   const calcDescription = `${currentResult} + ${enteredNumber}`; // it implicitly converts the value to string 
   // we also need to run it before the calculation so that we can get the previous currentResult
   currentResult = currentResult + enteredNumber; // parseFloat() could also be used but input cannot be a number with decimal points
