7ba55f06e85fdbb3a760252e02e2570e624dc905
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-10-03 22:47:05 +0200

    comment

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,7 +2,7 @@ const defaltResult = 0;
 let currentResult = defaltResult;
 
 function addition() {
-  currentResult = currentResult + parseInt(userInput.value);
+  currentResult = currentResult + parseInt(userInput.value); // parseFloat() could also be used but input cannot be a number with decimal points
   outputResult(currentResult, '');
 }
 
