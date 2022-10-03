45d06ae192bcd715510c992200340fbc30f4081a
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-10-03 22:54:13 +0200

    to avoid code repetition #1

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,9 +2,10 @@ const defaltResult = 0;
 let currentResult = defaltResult;
 
 function addition() {
-  const calcDescription = `${currentResult} + ${userInput.value}`; // it implicitly converts the value to string 
+  const enteredNumber = parseInt(userInput.value);
+  const calcDescription = `${currentResult} + ${enteredNumber}`; // it implicitly converts the value to string 
   // we also need to run it before the calculation so that we can get the previous currentResult
-  currentResult = currentResult + +userInput.value; // parseFloat() could also be used but input cannot be a number with decimal points
+  currentResult = currentResult + enteredNumber; // parseFloat() could also be used but input cannot be a number with decimal points
   outputResult(currentResult, calcDescription);
 }
 
